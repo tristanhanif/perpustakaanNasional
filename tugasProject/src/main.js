879f35fe3f5f614833b43e5eafea5a3410fbaf7a
tristanhanif <tristanhanif1769@gmail.com>
2025-05-15 08:39:41 +0700

    library

--- tugasProject/src/main.js
+++ tugasProject/src/main.js
@@ -1,6 +1,5 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import LibrarySistem from './LibrarySystem.vue'
-
+import LibrarySistem from './components/LibrarySistem.vue'
 createApp(LibrarySistem).mount('#app')
